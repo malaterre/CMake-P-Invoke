fdd7805eb185bc09b3047bc69b48a0a6b9a019d9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-11-24 11:49:48 +0100

    good first pass seesm to be working as expected

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,15 +1,103 @@
-﻿using System.Runtime.InteropServices;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApplication
 {
+    // inherits from SafeHandleZeroOrMinusOneIsInvalid, so IsInvalid is already implemented.
+    internal sealed class MySafeHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        // A default constructor is required for P/Invoke to instantiate the class
+        public MySafeHandle()
+            : base(ownsHandle: true)
+        {
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            NativeMethods.delete_dotnetstream(handle);
+            return true;
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        // Returns the SafeHandle instead of IntPtr
+        [DllImport("NativeLibrary")]
+        internal extern static MySafeHandle create_dotnetstream();
+
+        [DllImport("NativeLibrary")]
+        internal extern static void delete_dotnetstream(IntPtr handle);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        internal delegate int ReadFunctionDelegate(ref IntPtr buffer, int count);
+
+        [DllImport("NativeLibrary")]
+        internal extern static void setup_dotnetstream(MySafeHandle handle, IntPtr callback, int buffering);
+    }
+
+    public sealed class MyFileWrapper : IDisposable
+    {
+        private readonly Stream _input; // keep reference to avoid garabage collection
+        private readonly MySafeHandle _handle;
+
+        // System.IO.Stream expects an input byte[] to read into,
+        // since we do not want to go the `unsafe` road, create one here:
+        // also define the buffering implicitely:
+        // https://stackoverflow.com/questions/1862982/c-sharp-filestream-optimal-buffer-size-for-writing-large-files
+        private readonly byte[] _buffer = new byte[4096];
+
+        public MyFileWrapper(Stream input)
+        {
+            _input = input;
+            _handle = NativeMethods.create_dotnetstream();
+            NativeMethods.ReadFunctionDelegate rf = this.MyRead;
+            var ptr = Marshal.GetFunctionPointerForDelegate(rf);
+            NativeMethods.setup_dotnetstream(_handle, ptr, _buffer.Length);
+        }
+
+        private int MyRead(ref IntPtr outbuffer, int count)
+        {
+            // programmer error:
+            Debug.Assert(count == _buffer.Length);
+            try
+            {
+                int ret = _input.Read(_buffer, 0, _buffer.Length);
+                Marshal.Copy(_buffer, 0, outbuffer, ret);
+                return ret;
+            }
+            catch
+            {
+            // https://www.mono-project.com/docs/advanced/pinvoke/#runtime-exception-propagation
+            // https://github.com/dotnet/runtime/issues/4756
+
+                return -1;
+            }
+        }
+
+        // - There is no need to implement a finalizer, MySafeHandle already has one
+        // - You do not need to protect against multiple disposing, MySafeHandle already does
+        public void Dispose()
+        {
+            _handle.Dispose();
+        }
+    }
+
     class Program
     {
         [DllImport("NativeLibrary.dll")]
         public static extern void HelloWorld();
 
+        [DllImport("NativeLibrary.dll")]
+        public static extern void SetStream(IntPtr stream);
+
         static void Main(string[] args)
         {
             HelloWorld();
+            var fs = new FileStream("README.md", FileMode.Open);
+            var w = new MyFileWrapper(fs);
         }
     }
 }
--- NativeLibrary/Source.cpp
+++ NativeLibrary/Source.cpp
@@ -4,5 +4,76 @@ extern "C" __declspec(dllexport) void HelloWorld();
 
 void HelloWorld()
 {
-	std::cout << "Hello world from c++" << std::endl;
+	std::cout << "Hello world2 from c++" << std::endl;
+}
+
+extern "C" {
+    // System.IO.Stream
+    // public override int Read(byte[] buffer, int offset, int count)
+    typedef int (*ReadFunction)(char** buffer, int size);
+}
+
+namespace details {
+
+    class dotnetstream {
+    public:
+        ReadFunction readfunction_;
+        int buffersize;
+        int read(char* buffer) {
+            const int ret = (*readfunction_)(&buffer, buffersize);
+            return ret;
+        }
+    };
+
+    // https://stackoverflow.com/questions/14086417/how-to-write-custom-input-stream-in-c
+    class compressbuf
+        : public std::streambuf {
+        dotnetstream* stream_;
+        char* buffer_;
+        // context for the compression
+    public:
+        compressbuf(dotnetstream* stream)
+            : stream_(stream), buffer_(new char[stream->buffersize]) {
+            // initialize compression context
+        }
+        ~compressbuf() { delete[] this->buffer_; }
+        int underflow() {
+            if (this->gptr() == this->egptr()) {
+                // decompress data into buffer_, obtaining its own input from
+                // this->sbuf_; if necessary resize buffer
+                // the next statement assumes "size" characters were produced (if
+                // no more characters are available, size == 0.
+                const int size = this->stream_->read(this->buffer_);
+                this->setg(this->buffer_, this->buffer_, this->buffer_ + size);
+            }
+            return this->gptr() == this->egptr()
+                ? std::char_traits<char>::eof()
+                : std::char_traits<char>::to_int_type(*this->gptr());
+        }
+    };
+
+}
+
+extern "C" {
+ //   typedef void (*ReadFunction)(char* buffer, int size);
+
+    __declspec(dllexport) struct dotnetstream *create_dotnetstream() {
+        return reinterpret_cast<struct dotnetstream *>(new details::dotnetstream);
+    }
+    __declspec(dllexport) void delete_dotnetstream(struct dotnetstream* stream) {
+        delete reinterpret_cast<details::dotnetstream*>(stream);
+    }
+    __declspec(dllexport) void setup_dotnetstream(struct dotnetstream* stream, ReadFunction readFunction, int buffering) {
+        details::dotnetstream* s = reinterpret_cast<details::dotnetstream*>(stream);
+        s->readfunction_ = readFunction;
+        s->buffersize = buffering;
+
+        // play
+        if (readFunction) {
+            
+            details::compressbuf   sbuf(s);
+            std::istream  in(&sbuf);
+            std::cout << in.rdbuf() << std::endl;
+        }
+    }
 }
